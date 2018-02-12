ee7cea55b521b82038fb070e498fb9d37700348d
Renuja <Renuja@github.com>
2018-02-12 14:52:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/app/myapp/raise.java
@@ -0,0 +1,13 @@
+package com.example.app.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class raise extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.raise);
+    }
+}
